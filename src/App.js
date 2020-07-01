2c30eac2617b132a252504142e7b728e84eaacb8
Phi Dinh <phidv9855@gmail.com>
2020-07-01 11:35:55 +0700

    cards and chart resolve

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react'
 
+import { Cards, Chart, CountryPicker } from './components';
+import styles from './App.module.css';
+import { fetchData } from './api';
+
 class App extends Component {
+    state = {
+        data: {},
+    }
+    async componentDidMount() {
+        const fetchedData = await fetchData(); //return our function
+        this.setState({
+            data: fetchedData,
+        })
+    }
+
     render() {
+        const { data } = this.state;
         return (
-            <div>
-                
+            <div className={styles.container}>
+                <Cards data={data} />
+                <Chart />
+                <CountryPicker />
             </div>
         )
     }
